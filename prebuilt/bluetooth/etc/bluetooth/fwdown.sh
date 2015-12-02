150e09c19f5d055c2480e7506fe2afdbc28d7e83
Chanho Park <chanho61.park@samsung.com>
2015-12-02 11:32:58 +0900

    artik: bluetooth: update bcm4354 hcd
    This patch updates bcm4354 hcd to the latest version.
    This patch also supports parsing bluetooth bd_addr from commandline.
    
    Change-Id: I8109e16723a3597ff64441caedd200081ebdd0d5
    Signed-off-by: Chanho Park <chanho61.park@samsung.com>

--- prebuilt/bluetooth/etc/bluetooth/fwdown.sh
+++ prebuilt/bluetooth/etc/bluetooth/fwdown.sh
@@ -1,45 +1,60 @@
 #!/bin/sh
 
-function gen_bd_addr {
-        [ -d /opt/.bd_addr ] || rm -f /opt/.bd_addr
-        [ -d /etc/bluetooth/main.conf ] || rm -f /etc/bluetooth/main.conf
-
-        macaddr=$(dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/00:\1:\2:\3:\4:\5/')
-        echo $macaddr > /opt/.bd_addr
-        chmod 400 /opt/.bd_addr
+function gen_bluez_conf {
 cat > /etc/bluetooth/main.conf << EOF
 [General]
 Name = $1
 EOF
-        sync
+	sync
+}
+
+function gen_bd_addr {
+	[ -d /opt/.bd_addr ] || rm -f /opt/.bd_addr
+
+	macaddr=$(dd if=/dev/urandom bs=1024 count=1 2>/dev/null|md5sum|sed 's/^\(..\)\(..\)\(..\)\(..\)\(..\).*$/00:\1:\2:\3:\4:\5/')
+	echo $macaddr > /opt/.bd_addr
+	chmod 400 /opt/.bd_addr
+	sync
 }
 
 ARTIK5=`cat /proc/cpuinfo | grep -i EXYNOS3`
 
 if [ "$ARTIK5" != "" ]; then
-        TTY_NUM=0
+	TTY_NUM=0
 	ARTIK_DEV=ARTIK5
 else
-        TTY_NUM=2
+	TTY_NUM=2
 	ARTIK_DEV=ARTIK10
 fi
 
-if [ ! -f "/opt/.bd_addr" ]; then
-        gen_bd_addr ${ARTIK_DEV}
-fi
+[ -d /etc/bluetooth/main.conf ] || gen_bluez_conf $ARTIK_DEV
 
-BD_ADDR=`cat /opt/.bd_addr`
+for x in $(cat /proc/cmdline); do
+	case $x in
+	bd_addr=*)
+		BD_ADDR=${x#bd_addr=}
+		;;
+	esac
+done
+
 if [ "$BD_ADDR" == "" ]; then
-        gen_bd_addr ${ARTIK_DEV}
-        BD_ADDR=`cat /opt/.bd_addr`
+	if [ ! -f "/opt/.bd_addr" ]; then
+		gen_bd_addr ${ARTIK_DEV}
+	fi
+
+	BD_ADDR=`cat /opt/.bd_addr`
+	if [ "$BD_ADDR" == "" ]; then
+		gen_bd_addr ${ARTIK_DEV}
+		BD_ADDR=`cat /opt/.bd_addr`
+	fi
 fi
 
 pushd `dirname $0`
 
-./brcm_patchram_plus --patchram ./BCM4354_003.001.012.0301.0000_Samsung_Artik_TEST_ONLY.hcd \
+./brcm_patchram_plus --patchram BCM4354_003.001.012.0353.0745_Samsung_Artik_ORC.hcd \
 	--no2bytes --baudrate 3000000 \
 	--use_baudrate_for_download /dev/ttySAC${TTY_NUM} \
 	--bd_addr ${BD_ADDR} \
-	--enable_hci &
+	--enable_hci > /dev/null &
 
 echo $! > /run/brcm_patchram_plus.pid
